e6f4d18c5d9bdb9938d1436b9b3fa867979a4ed0
kunal-216 <kunaltalreja216@gmail.com>
2024-09-12 22:04:15 +0530

    first commit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,17 @@
+import { useNavigate } from "react-router-dom"
+import { Button } from "./ui/button"
+
 const Header = () => {
+
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate("/user")
+  }
+
   return (
     <div>
-      
+      <Button onClick={handleButtonClick}>Sign Up</Button>
     </div>
   )
 }
