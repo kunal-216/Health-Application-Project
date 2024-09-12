aa7a627f606faf6ac07081796800993a46973f9e
kunal-216 <kunaltalreja216@gmail.com>
2024-09-12 18:33:49 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-import "./App.css"
 import {Routes, Route} from "react-router-dom"
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
